60589f162fb9ebca22b409723eb65141562ab6bb
sooraj117 <soorajbab2@gmail.com>
2020-07-17 10:53:50 +0530

    My task is completed

---
+++ src/test/java/org/prodapt/SpiceJet.java
@@ -0,0 +1,31 @@
+package org.prodapt;
+
+import org.testng.annotations.Test;
+
+public class SpiceJet {
+	
+	@Test
+	public static void TC1() {
+		
+		
+	}
+	
+	@Test
+	public static void TC2() {
+		
+		
+	}
+	
+	@Test
+	public static void TC3() {
+		
+		
+	}
+	
+	@Test
+	public static void TC4() {
+		
+		
+	}
+
+}
